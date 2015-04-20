e3200fc781dd7da908ffbffddda62dca33c70f86
Shanki <ssg.bnv@gmail.com>
2015-04-20 10:02:25 +0530

    updated fabfile

--- tools/fabfile.py
+++ tools/fabfile.py
@@ -1,24 +1,31 @@
-from fabric.api import env, run, sudo, cd, get, put
+from fabric.api import env, run, sudo, cd
 
 env.user = 'ubuntu'
-env.hosts = ["ec2-54-251-162-124.ap-southeast-1.compute.amazonaws.com"]
-env.key_filename = '/home/shabi/Desktop/doc/serv/myubuntu.pem'
+env.hosts = [] # specify host
+env.key_filename = '' # specify aws pem file
+
 
 def install_git():
     sudo("apt-get install git")
-    
+
+
 def checkout_git(git_ssh_path):
     with cd("/home/proj"):
         sudo("git clone %s" % git_ssh_path)
-    
-def install_mongodb():
+
+
+def setup_mongodb():
     sudo('apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10')
     sudo('echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list')
     sudo('apt-get update')
     sudo('apt-get install -y mongodb-org')
+
     
-def install_nginx():
+def setup_nginx():
     sudo('apt-get install nginx')
+    sudo("cp /home/proj/directory/tools/app_nginx /etc/nginx/sites-available/")
+    sudo("service nginx restart")
+
     
 def install_requirements(req_path):
     sudo('apt-get update')
@@ -27,12 +34,34 @@ def install_requirements(req_path):
     sudo("sudo apt-get install zlib1g-dev")
     sudo("apt-get install libmemcached-dev")
     sudo("pip install -r %s" % req_path)
+
     
 def setup_supervisor():
     sudo("apt-get install supervisor")
     sudo("cp /home/proj/directory/tools/gunicorn.conf /etc/supervisor/conf.d/")
-    sudo("service supervisord restart")
+    sudo("service supervisor restart")
+    
     
 def set_up():
-    install_mongodb()
-    install_nginx()
+    """
+    Install requirements and setup application
+    """
+    
+    install_git()
+    checkout_git()
+    setup_mongodb()
+    install_requirements()
+    setup_supervisor()
+    setup_nginx()
+
+
+def app_update():
+    """
+    It will pull records on server and restart servers
+    """
+    
+    with cd("/home/proj/directory"):
+        sudo("git pull origin master")
+    
+    sudo("service supervisor restart")
+    sudo("service nginx restart")
